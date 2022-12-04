f54e79ae65cc386c5793a28158e868b2d3f1c540
Артем Хакимов <arthack00@gmail.com>
2022-12-04 11:20:24 +0500

    just in case

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    '''Тренировка атаки'''
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон '
                 f'противнику равный {5 + randint(3, 5)}')
@@ -14,6 +16,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    '''Тренировка защиты'''
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +26,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    '''Тренировка специальных умений'''
     if char_class == 'warrior':
         return (f'{char_name} применил специальное '
                 f'умение «Выносливость {80 + 25}»')
@@ -35,6 +39,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    '''Выбор вида тренировки персонажа'''
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -59,6 +64,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    '''Описание и выбор персонажа'''
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -81,6 +87,8 @@ def choice_char_class() -> str:
 
 
 def main() -> None:
+    '''Двигатель игры'''
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -92,5 +100,6 @@ def main() -> None:
     print(start_training(char_name, char_class))
 
 
+if __name__ == '__main__':
+    main()
 
-main()
--- temp.py
+++ temp.py
@@ -1,5 +1,23 @@
-def we_crash_all(name: str) -> str:
-    return 'Привет, ' + name + ', мы всё сломали!'
+from math import sqrt
+
+message = ('Добро пожаловать в самую лучшую программу для вычисления '
+           'квадратного корня из заданного числа')
+print(message)
 
 
-print(we_crash_all(5))
+def calculate_square_root(Number):
+    """Вычисляет квадратный корень."""
+    return sqrt(Number)
+
+
+def calc(your_number):
+    """Вычисляет и выводит квадратный корень."""
+    if your_number <= 0:
+        return
+    root = calculate_square_root(your_number)
+    print(f'Мы вычислили квадратный корень из введённого вами числа. '
+          f'Это будет: {root}')
+
+
+print(message)
+calc(25.5)
